12e8999def885db5fa254697e19c62b1cc1fb001
Jason J <iam@ovrhere.com>
2014-08-14 20:45:38 -0400

    Added major setup to provider, configured flipper in layout
    [Mod][Add]: strings: Provided the strings "no text set". As well as
    "CALL" string for future use.
    [Mod]: pic_info_card_appwidget:
    -Configured the AdapterViewFlipper widget
    -Added the empty text view
    -Removed some old comments
    [Mod]: PictureInfoCardWidgetProvider: Add the starting point of the
    widget provider providing setup.
    [Mod]: Added some grey to colors.

--- src/com/ovrhere/android/pictureinfocard/widget/provider/PictureInfoCardWidgetProvider.java
+++ src/com/ovrhere/android/pictureinfocard/widget/provider/PictureInfoCardWidgetProvider.java
@@ -15,15 +15,94 @@
  */
 package com.ovrhere.android.pictureinfocard.widget.provider;
 
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.widget.RemoteViews;
+
+import com.ovrhere.android.pictureinfocard.widget.R;
+import com.ovrhere.android.pictureinfocard.widget.prefs.PreferenceUtils;
+import com.ovrhere.android.pictureinfocard.widget.remoteviews.AdapterViewFlipperWidgetService;
+import com.ovrhere.android.pictureinfocard.widget.ui.PICWidgetConfigurationActivity;
 
 /**
  * The {@link AppWidgetProvider} for the widget. 
  * Provides an interface for widget interactions (deletions, creations, updates, etc).
  *  
  * @author Jason J.
- * @version 0.1.0-20140806
+ * @version 0.3.0-20140812
  */
 public class PictureInfoCardWidgetProvider extends AppWidgetProvider {
+	/** Class name for debugging purposes. */
+	@SuppressWarnings("unused")
+	final static private String CLASS_NAME = 
+			PictureInfoCardWidgetProvider.class.getSimpleName();
+	
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+			int[] appWidgetIds) {
+		super.onUpdate(context, appWidgetManager, appWidgetIds);
+		
+        final int SIZE = appWidgetIds.length;
+        for (int i = 0; i < SIZE; i++) {
+            int appWidgetId = appWidgetIds[i];
+        
+            RemoteViews remoteViews = 
+    				new RemoteViews( context.getPackageName(), R.layout.pic_info_card_appwidget);
+            
+	        Intent launchIntent = new Intent(context, PICWidgetConfigurationActivity.class); 
+	        launchIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+	        
+	        PendingIntent pendingIntent = 
+	        		PendingIntent.getActivity(	context,
+	        				//unique request code (or uri) required for unique pendingintent 
+	        									appWidgetId /* requestCode */, 
+	        									launchIntent, 
+	        									0 /* flags */);
+	        
 
+	        remoteViews.setOnClickPendingIntent(R.id.com_ovrhere_picwidget_widget_layout, 
+	        									pendingIntent);
+	        //pending intent complete.
+	        
+	        Intent flipperIntent = 
+	        		new Intent(context, AdapterViewFlipperWidgetService.class);
+	        
+	        flipperIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+	        // extras are ignored, so we need to embed the extras to be not ignored
+	        flipperIntent.setData(Uri.parse(flipperIntent.toUri(Intent.URI_INTENT_SCHEME)));
+
+            
+	        remoteViews.setRemoteAdapter(
+	        		R.id.com_ovrhere_picwidget_widget_viewAnimator_infoText, 
+	        		flipperIntent);	        
+            // empty view to displayed when adapter is empty.
+	        remoteViews.setEmptyView(
+	        		R.id.com_ovrhere_picwidget_widget_viewAnimator_infoText, 
+	        		R.id.com_ovrhere_picwidget_widget_text_noText);
+	        
+	        
+	        //clear remote views
+	        appWidgetManager.updateAppWidget( appWidgetId, null ); 
+        	appWidgetManager.updateAppWidget( appWidgetId, remoteViews );
+        }
+	}
+	
+	@Override
+	public void onDeleted(Context context, int[] appWidgetIds) {
+		super.onDeleted(context, appWidgetIds);
+		
+		final int SIZE = appWidgetIds.length;
+        for (int i = 0; i < SIZE; i++) {
+            int appWidgetId = appWidgetIds[i];
+            SharedPreferences.Editor editor = 
+            		PreferenceUtils.getPreferences(context, appWidgetId).edit();
+            //clear all preferences
+            editor.clear().commit();
+        }
+	}
 }
